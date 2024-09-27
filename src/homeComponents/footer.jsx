d9b4ff31ba5efaf347d8f4398f62e3680325b7e8
Abhishek Kumar <ak0188644@gmail.com>
2024-09-27 19:24:12 +0530

    Update footer.jsx

--- src/homeComponents/footer.jsx
+++ src/homeComponents/footer.jsx
@@ -3,25 +3,62 @@ import GitHubButton from 'react-github-btn'
 import "./style.css";
 class Footer extends Component {
     render() {
+        const currentYear = new Date().getFullYear();
+
         return (
-            <footer class="page-footer font-small special-color-dark pt-4 ">
-
-                <div className='flex-wrap' style={{textAlign:"center"}}>
-                    <div className='m-2'><GitHubButton href="https://github.com/TamimEhsan" data-show-count="true" aria-label="Follow @TamimEhsan on GitHub">Follow @TamimEhsan</GitHubButton></div>
-                    <GitHubButton href="https://github.com/TamimEhsan/Pathfinder-2.0" data-icon="octicon-star" data-show-count="true" aria-label="Star TamimEhsan/Pathfinder-2.0 on GitHub">Star</GitHubButton>
-                    &nbsp
-                    <GitHubButton href="https://github.com/TamimEhsan/Pathfinder-2.0/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork TamimEhsan/Pathfinder-2.0 on GitHub">Fork</GitHubButton>
-                    &nbsp
-                    <img src="https://visitor-badge.laobi.icu/badge?page_id=TamimEhsan.AlgorithmVisualizer" alt="Hits"/>
+            <footer className="page-footer font-small special-color-dark pt-4">
+                <div className='flex-wrap' style={{ textAlign: "center" }}>
+                    <a 
+                        href="https://www.facebook.com/profile.php?id=100028091352527&sk=grid" 
+                        className="m-2 social-icon" 
+                        target = "_blank"
+                        aria-label="Follow Abhishek Kumar on Facebook">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" alt="Facebook" />
+                    </a>
+                    <a 
+                        href="https://www.instagram.com/this_isabhi07" 
+                        className="m-2 social-icon" 
+                        target = "_blank"
+                        aria-label="Follow Abhishek Kumar on Instagram">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png" alt="Instagram" />
+                    </a>
+                    <a 
+                        href="https://twitter.com/coder_abhi07" 
+                        className="m-2 social-icon" 
+                        target = "_blank"
+                        aria-label="Follow Abhishek Kumar on Twitter">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/twitter.png" alt="Twitter" />
+                    </a>
+                    <a 
+                        href="https://www.linkedin.com/in/abhishek-kumar-573418164/" 
+                        className="m-2 social-icon" 
+                        target = "_blank"
+                        aria-label="Follow Abhishek Kumar on LinkedIn">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="LinkedIn" />
+                    </a>
                 </div>
-
-                <div class="footer-copyright text-center py-3">© 2020 Copyright:
-                    <a href="https://github.com/tamimehsan" style={{color: "#65b6f0"}}> Tamim Ehsan</a>
+    
+                <div className="footer-copyright text-center py-3">
+                    © {currentYear} Abhishek Kumar
                 </div>
-
+    
+                <style jsx>{`
+                    .social-icon img {
+                        width: 25px;
+                        height: 25px;
+                        margin: 10px 0px;
+                        transition: transform 0.2s, filter 0.2s;
+                    }
+    
+                    .social-icon:hover img {
+                        transform: scale(1.2);
+                        filter: brightness(0.8);
+                    }
+                `}</style>
             </footer>
         );
     }
+    
 }
 
 export default Footer;
